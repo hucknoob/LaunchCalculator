b744af5df9ddc1c3f43cfb40b951ddc85557182d
hucknoob <noob1998ololo@gmail.com>
2022-07-11 13:00:44 +0300

    launch calculator

--- app/src/main/java/com/example/launchcalculator/MainActivity.java
+++ app/src/main/java/com/example/launchcalculator/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.launchcalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.open_calculator).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("calc://launch"));
+                intent.putExtra("msg", "Hello");
+                startActivity(Intent.createChooser(intent, null));
+            }
+        });
     }
 }
